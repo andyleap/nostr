0c953125b27d45decf5c756001bcb972ac8b3f19
Andy Leap <andyleap@gmail.com>
2023-04-27 10:33:37 -0700

    Add host to connection string

--- cmd/relay/main.go
+++ cmd/relay/main.go
@@ -10,11 +10,12 @@ import (
 )
 
 func main() {
+	pgHost := os.Getenv("PG_HOST")
 	pgUser := os.Getenv("PG_USER")
 	pgPass := os.Getenv("PG_PASS")
 	pgDB := os.Getenv("PG_DB")
 
-	pgConnString := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", pgUser, pgDB, pgPass)
+	pgConnString := fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=disable", pgHost, pgUser, pgDB, pgPass)
 
 	store, err := postgres.New(pgConnString)
 	if err != nil {
