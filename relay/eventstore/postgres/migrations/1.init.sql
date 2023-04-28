ad061b14330ac4afe5f374a6fe993e65e41e0e9b
Andy Leap <andyleap@gmail.com>
2023-04-27 22:04:18 -0700

    Fix index creation

--- relay/eventstore/postgres/migrations/1.init.sql
+++ relay/eventstore/postgres/migrations/1.init.sql
@@ -9,10 +9,11 @@ CREATE TABLE IF NOT EXISTS events (
     mungedTags JSONB NOT NULL,
     content TEXT NOT NULL,
     sig CHAR(128) NOT NULL,
-    PRIMARY KEY (id, pubkey),
-    INDEX (kind),
-    INDEX (id),
-    INDEX (pubkey),
+    PRIMARY KEY (id, pubkey)
 );
+
+CREATE INDEX IF NOT EXISTS events_kind ON events (kind);
+CREATE INDEX IF NOT EXISTS events_id ON events (id);
+CREATE INDEX IF NOT EXISTS events_pubkey ON events (pubkey);
 
 COMMIT;
